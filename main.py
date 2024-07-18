833a03921982af85c42866de5303065720e0ffc1
I4ze <Iaze.guilherme25@gmail.com>
2024-07-17 22:22:49 -0300

    atualiza script para nao travar o servidor

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI, File, UploadFile
 import whisper
 import os
+import asyncio
 
 app = FastAPI()
 
-model = whisper.load_model("base")
+model = whisper.load_model("tiny")
 
 @app.post("/transcrever/")
 async def transcrever(file: UploadFile = File(...)):
     audio_path = f"temp_{file.filename}"
     with open(audio_path, "wb") as buffer:
-        buffer.write(file.file.read())
-    result = model.transcribe(audio_path)
+        buffer.write(await file.read())
+    loop = asyncio.get_event_loop()
+    result = await loop.run_in_executor(None, model.transcribe, audio_path)
     os.remove(audio_path)
-
+    
     return {"transcricao": result["text"]}
